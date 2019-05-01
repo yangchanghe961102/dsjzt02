cd067471cf3dcb8a785635d086e383740d878e56
yanda.li <15810271029@163.com>
2019-05-01 17:33:14 +0800

    user_info

--- js/user_info.js
+++ js/user_info.js
@@ -8,6 +8,7 @@ function load_user_info() {
         .attr("height", height)
         .attr("id", "user_info_svg")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
     text_width = width * 0.6;
     text_height = height;
     var info_text = svg.append("g")
@@ -21,26 +22,39 @@ function load_user_info() {
         .attr("font-size", text_height * 0.07);
 
     info_text.append("text")
-        .text("您已去过  " + "20" + "  个城市")
+        .text("您已去过  " + CITY_NUM + "  个城市")
         .attr("x", 0)
         .attr("y", text_height * 0.3)
         .attr("font-size", text_height * 0.07);
 
     info_text.append("text")
         .text("当前想去城市： ")
+        .attr("id", "city")
         .attr("x", 0)
         .attr("y", text_height * 0.5)
         .attr("font-size", text_height * 0.07);
 
-    info_text.append("text")
-        .text("北京                厦门                上海")
-        .attr("x", 0)
-        .attr("y", text_height * 0.67)
-        .attr("font-size", text_height * 0.07);
+    for (i = 0; i < 6; ++i) {
+        info_text.append("text")
+            .text(city_list[i])
+            .attr("x", function(d){
+                return (i % 3) * width * 0.25;
+            })
+            .attr("y", function(d){
+                return text_height * (parseInt(i / 3) * 0.13 + 0.67);
+            })
+            .attr("font-size", text_height * 0.07);
+    }
 
-    info_text.append("text")
-        .text("杭州                南京                成都")
-        .attr("x", 0)
-        .attr("y", text_height * 0.8)
-        .attr("font-size", text_height * 0.07);
+
+    head_width = width * 0.3;
+    head_height = height * 0.5;
+    head_size = Math.min(head_width, head_height);
+    var head_image = svg.append("g").append("image")
+        .attr("xlink:href", USERHEAD)
+        .attr("id", "head_image")
+        .attr("width", head_size * 0.9)
+        .attr("height", head_size * 0.9)
+        .attr("x", text_width + (head_width - head_size) / 2)
+        .attr("y", (head_height - head_size) / 2);
 }
